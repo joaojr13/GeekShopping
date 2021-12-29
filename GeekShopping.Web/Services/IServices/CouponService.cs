c502ad8856bda643ca37f672202794b2fad40399
João A. Junior <joao.jr13@icloud.com>
2021-12-29 20:29:26 -0300

    Criação do CouponService

---
+++ GeekShopping.Web/Services/IServices/CouponService.cs
@@ -0,0 +1,26 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Utils;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public class CouponService : ICouponService
+    {
+        private readonly HttpClient _httpClient;
+        public const string BasePath = "api/v1/coupon";
+
+        public CouponService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<CouponViewModel> GetCoupon(string code, string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var response = await _httpClient.GetAsync($"{BasePath}/{code}");
+            if (response.StatusCode != HttpStatusCode.OK) return new CouponViewModel();
+            return await response.ReadContentAsync<CouponViewModel>();
+        }
+    }
+}
