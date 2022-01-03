c09cc099397288c2df08a42d126e2cf307d0ab62
João A. Junior <joao.jr13@icloud.com>
2022-01-02 23:39:31 -0300

    Desenvolvimento Payment API

---
+++ GeekShopping.PaymentAPI/PaymentConsumer/RabbitMQPaymentConsumer.cs
@@ -0,0 +1,79 @@
+﻿using GeekShopping.PaymentAPI.Messages;
+using GeekShopping.PaymentAPI.RabbitMQSender;
+using GeekShopping.PaymentProcessor;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace GeekShopping.PaymentAPI.CheckoutConsumer
+{
+    public class RabbitMQPaymentConsumer : BackgroundService
+    {
+        private IConnection _connection;
+        private IModel _channel;
+        private IRabbitMQMessageSender _rabbitMQMessageSender;
+        private readonly IProcessPayment _processPayment;
+
+        public RabbitMQPaymentConsumer(IProcessPayment processPayment, IRabbitMQMessageSender rabbitMQMessageSender)
+        {
+            _rabbitMQMessageSender = rabbitMQMessageSender;
+            _processPayment = processPayment;
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "orderpaymentprocessqueue",
+                                  durable: false,
+                                  exclusive: false,
+                                  autoDelete: false,
+                                  arguments: null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (channel, evt) => 
+            {
+                var content = Encoding.UTF8.GetString(evt.Body.ToArray());
+
+                PaymentMessage? vo = JsonSerializer.Deserialize<PaymentMessage>(content);
+                ProcessPayment(vo).GetAwaiter().GetResult();
+
+                _channel.BasicAck(evt.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume("orderpaymentprocessqueue", autoAck: false, consumer);
+            return Task.CompletedTask;
+        }
+
+        private Task ProcessPayment(PaymentMessage? vo)
+        {
+            var result = _processPayment.PaymentProcessor();
+
+            UpdatePaymentResultMessage paymentResult = new()
+            {
+                Status = result,
+                OrderId = vo.OrderId,
+                Email = vo.Email
+            };
+
+            try
+            {
+                 _rabbitMQMessageSender.SendMessage(paymentResult);
+                 return Task.CompletedTask;
+            }
+            catch (Exception)
+            {
+                //log
+                throw;
+            }
+        }
+    }
+}
---
+++ GeekShopping.PaymentAPI/RabbitMQSender/RabbitMQMessageSender.cs
@@ -0,0 +1,97 @@
+﻿using GeekShopping.PaymentAPI.Messages;
+using GeekShopping.MessageBus;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace GeekShopping.PaymentAPI.RabbitMQSender
+{
+    public class RabbitMQMessageSender : IRabbitMQMessageSender
+    {
+        private readonly string _hostName;
+        private readonly string _password;
+        private readonly string _userName;
+        private IConnection _connection;
+        private const string ExchangeName = "DirectPaymentUpdateExchange";
+        private const string PaymentEmailUpdateQueueName = "PaymentEmailUpdateQueue";
+        private const string PaymentOrderUpdateQueueName = "PaymentOrderUpdateQueue";
+
+        public RabbitMQMessageSender()
+        {
+            _hostName = "localhost";
+            _password = "guest";
+            _userName = "guest";
+        }
+
+        public void SendMessage(BaseMessage message)
+        {
+            if (ConnectionExists())
+            {
+                using var channel = _connection.CreateModel();
+
+                channel.ExchangeDeclare(ExchangeName, 
+                                        ExchangeType.Direct, 
+                                        durable: false);
+
+                channel.QueueDeclare(PaymentEmailUpdateQueueName, false, false, false, null);
+                channel.QueueDeclare(PaymentOrderUpdateQueueName, false, false, false, null);
+
+                channel.QueueBind(PaymentEmailUpdateQueueName, ExchangeName, "PaymentEmail");
+                channel.QueueBind(PaymentOrderUpdateQueueName, ExchangeName, "PaymentOrder");
+
+
+                byte[] body = GetMessageAsByteArray(message);
+
+                channel.BasicPublish(exchange: ExchangeName,
+                                     routingKey: "PaymentEmail",
+                                     basicProperties: null, 
+                                     body: body); 
+
+                channel.BasicPublish(exchange: ExchangeName,
+                                     routingKey: "PaymentOrder",
+                                     basicProperties: null, 
+                                     body: body);
+            }
+
+        }
+
+        private byte[] GetMessageAsByteArray(BaseMessage message)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+            var json = JsonSerializer.Serialize((UpdatePaymentResultMessage)message, options);
+
+            var body = Encoding.UTF8.GetBytes(json);
+
+            return body;
+        }
+
+        private bool ConnectionExists()
+        {
+            if (_connection != null) return true;
+            CreateConnection();
+            return _connection != null;
+        }
+
+        private void CreateConnection()
+        {
+            try
+            {
+                var factory = new ConnectionFactory
+                {
+                    HostName = _hostName,
+                    UserName = _userName,
+                    Password = _password
+                };
+
+                _connection = factory.CreateConnection();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
